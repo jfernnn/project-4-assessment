3dbfaa903b6f2067a4538680066e7f04743f2a26
Joshua Fernelius <joshfern24@Joshuas-MacBook-Pro.local>
2020-07-17 14:13:16 -0600

    Updates

---
+++ precache-manifest.6d4783c7e039300e0f31bbaa085dc5f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c8b87dde9af69785c66ffc917ae740",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "8bd085008fa63092e988",
+    "url": "/project-4-assessment/static/css/main.a7084673.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd085008fa63092e988",
+    "url": "/project-4-assessment/static/js/main.622e4988.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
